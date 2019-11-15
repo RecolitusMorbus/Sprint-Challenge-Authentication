1d5765df1efe64aa56d9932c102677be9dafb8b8
Nolan Allen <nolan.r.allen@gmail.com>
2019-11-15 10:46:40 -0600

    2nd commit: initialized project and finished models

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Jokes = require('../jokes/jokes-model.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+
+  Jokes.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json({ err: `An error occurred.` });
+    });
 });
 
 router.post('/login', (req, res) => {
